7023e6d659c63c92de4a30771c76136e20991339
sushya123 <sushantsalunkhe581@gmail.com>
2025-03-02 21:02:11 +0530

    Add files via upload

---
+++ customer12/customer12/src/main/java/com/example/customer12/Customer12Application.java
@@ -0,0 +1,13 @@
+package com.example.customer12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Customer12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Customer12Application.class, args);
+	}
+
+}
---
+++ customer12/customer12/src/test/java/com/example/customer12/Customer12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customer12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Customer12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
